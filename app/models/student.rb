3c125c3aa5af56fd4af9bc0f04b04fed80396c9a
Chloe Steffens <caforonda103@yahoo.com>
2020-08-31 18:00:00 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,7 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  validates :name, presence: true
+  validates :hometown, presence: true
+  validates :birthday, presence: true
 
   def self.search(query)
-    query.present? ? self.all.select { |student| student.name.downcase.include?(query.downcase) } : self.all
+    if query.present?
+      where('NAME like ?', "%#{query}%")
+    else
+      self.all
+    end
   end
+end
