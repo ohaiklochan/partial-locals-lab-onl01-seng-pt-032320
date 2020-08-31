ac7a24de2348ce252b44af2b623c95a0c6a127fb
Chloe Steffens <caforonda103@yahoo.com>
2020-08-31 17:57:20 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,7 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.find(params[:query])
+    @students = Student.find(params[:id])
   end
 
   def student_params
